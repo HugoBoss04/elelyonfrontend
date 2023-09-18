4e66147273a62dcfcc31f5302f015ad96e09f501
Hugo <hugodev1997@gmail.com>
2023-09-17 20:35:48 -0500

    UI Fix #18

--- components/Contact.js
+++ components/Contact.js
@@ -9,52 +9,30 @@ const Contact = () => {
     setIsDesktop(window.innerWidth >= 1000);
   }, []);
   return (
-    <div className={classes.container}>
-      {!isDesktop ? (
-        <>
-          <div className={classes.left}>
-            <FaEnvelope size={22} className={classes.icon} />
-            <p className={classes.value}>ariana.hernandez@salonlofts.com</p>
-          </div>
-          <div className={classes.middle}>
-            <FaPhoneAlt size={22} className={classes.icon} />
-            <a href="tel:+1-713-560-5371" className={classes.value}>
-              713-560-5371
-            </a>
-          </div>
-          <div className={classes.right}>
-            <FaMapMarkerAlt size={22} className={classes.value} />
-            <p className={`${classes.value} ${classes['right-value']}`}>
-              23541 Westheimer Pkwy, Katy, TX 77494 - Loft 2
-            </p>
-          </div>
-        </>
-      ) : (
-        <div className={classes['content-container']}>
-          <div className={classes.left}>
-            <FaEnvelope size={22} className={classes.icon} />
-            <p className={classes.value}>ariana.hernandez@salonlofts.com</p>
-          </div>
-          <div className={classes.middle}>
-            <FaPhoneAlt size={22} className={classes.icon} />
-            <a href="tel:+1-713-560-5371" className={classes.value}>
-              <span
-                className={`${classes['number-fix']} ${classes['telephone-number']}`}
-              >
-                713-560-5371
-              </span>
-            </a>
-          </div>
-          <div className={classes.right}>
-            <FaMapMarkerAlt size={22} className={classes.value} />
-            <p className={`${classes.value} ${classes['right-value']}`}>
-              <span className={classes['number-fix']}>23541</span> Westheimer
-              Pkwy, Katy, TX{' '}
-              <span className={classes['number-fix']}>77494</span>
-            </p>
-          </div>
+    <div className={classes['contact-bg']}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <FaEnvelope size={22} className={classes.icon} />
+          <p className={classes.value}>ariana.hernandez@salonlofts.com</p>
         </div>
-      )}
+        <div className={classes.middle}>
+          <FaPhoneAlt size={22} className={classes.icon} />
+          <a href="tel:+1-713-560-5371" className={classes.value}>
+            <span
+              className={`${classes['number-fix']} ${classes['telephone-number']}`}
+            >
+              713-560-5371
+            </span>
+          </a>
+        </div>
+        <div className={classes.right}>
+          <FaMapMarkerAlt size={22} className={classes.value} />
+          <p className={`${classes.value} ${classes['right-value']}`}>
+            <span className={classes['number-fix']}>23541</span> Westheimer
+            Pkwy, Katy, TX <span className={classes['number-fix']}>77494</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
